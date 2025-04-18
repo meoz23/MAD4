8eb4a74ef04516d4eab65c01e183027c80d06177
meoz23 <laboumyz@gmail.com>
2025-04-18 16:09:25 -0400

    Update main.dart to initialize Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+import 'auth_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,12 +14,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Message Board App',
-      debugShowCheckedModeBanner: false,
+      title: 'Flutter App',
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        primarySwatch: Colors.blue,
       ),
-      home: const SplashScreen(),
+      home: const AuthScreen(),
     );
   }
 }
